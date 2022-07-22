d2787355f19c17f4e2855d1e871343e4917717ee
Matthew Stroble <msstrob@auburn.edu>
2022-07-21 20:33:05 -0700

    custom trees

--- cyber scenarios/sshvsftp/primitive.py
+++ cyber scenarios/sshvsftp/primitive.py
@@ -1,21 +1,8 @@
 from genotype import *
 import random
 import socket 
-import grpc
 import os.path
-import actions_pb2_grpc
-import actions_pb2
 import time
-
-def PerformRemoteAction(stub, host, port, username, password, command):
-    request = actions_pb2.RemoteExecutionRequest()
-    request.host = host
-    request.port = port
-    request.username = username
-    request.password = password
-    request.command = command
-    response = stub.RemoteExecution(request)
-    return response
 
 # defining types of agents as constant strings
 ATTACKER = 'attacker'
@@ -24,12 +11,15 @@ ATTACKER = 'attacker'
 IP_ACTION = 'ip action'
 IP_ADDRESS = 'ip address'
 
-# Server address of gRPC server
-SERVER_ADDRESS = "localhost:23350"
+SERVICE = 'service'
+SSH = 'ssh'
+SFTP ='sftp'
 
-# only attackers can use this primitive, it's data type is IP_ACTION, and it 
-# requires 2 IP_ACTION-type children nodes.
-@GeneticTree.declarePrimitive(ATTACKER, IP_ACTION, (IP_ACTION, IP_ACTION))
+
+SSHACTIONS = 'sshactions'
+
+
+@GeneticTree.declarePrimitive(ATTACKER, SERVICE, (SFTP, SSH))
 def hard_coded_range_if(self, input_nodes, context):
 	ip_address = context['ip address'] 	# we assume the provided context
 	service = context['service']		# parameter is a Dict with 'ip address'
@@ -47,30 +37,70 @@ def hard_coded_range_if(self, input_nodes, context):
 # only attackers can use this primitive, it's data type is IP_ACTION, and it
 # does not have any children (so it's a leaf node)
 # ssh login attack
-@GeneticTree.declarePrimitive(ATTACKER, IP_ACTION, ())
+@GeneticTree.declarePrimitive(ATTACKER, SSH, (SSHACTIONS,))
+def hard_coded_range_if(self, input_nodes, context):
+	ip_address = context['ip address'] 	# we assume the provided context
+	service = context['service']		# parameter is a Dict with 'ip address'
+	# Validate IP range
+
+	if service == "ssh":
+		input_nodes[0].execute(context)
+		# sftp
+	elif service == "sftp":
+		input_nodes[1].execute(context)
+	else:
+		# send to new tree
+		exit(1)
+
+@GeneticTree.declarePrimitive(ATTACKER, SSHACTIONS, ())
 def action0(self, input_nodes, context):
 	ip_address = context['ip address'] 	# we assume the provided context
 										# parameter is a Dict with 'ip address'
-
-	with grpc.insecure_channel(SERVER_ADDRESS) as channel:
-		# create a stub (client)
-		stub = actions_pb2_grpc.ActionsStub(channel)
-		#recieved = PerformAction(stub)
-
-		# Remote Action Demo
-		targetIP = "192.168.1.200"
-		targetPort = 22
-		targetUsername = "pi"
-		targetPassword = ""
-		targetCommand = "ls"
-		response = PerformRemoteAction(stub, targetIP, targetPort, 
-										targetUsername, targetPassword, targetCommand)
 	print('Chose SSH', ip_address)
 
-# only attackers can use this primitive, it's data type is IP_ACTION, and it
-# does not have any children (so it's a leaf node)
-@GeneticTree.declarePrimitive(ATTACKER, IP_ACTION, ())
-def action1(self, input_nodes, context):
+@GeneticTree.declarePrimitive(ATTACKER, SSHACTIONS, ())
+def action0(self, input_nodes, context):
+	ip_address = context['ip address'] 	# we assume the provided context
+										# parameter is a Dict with 'ip address'
+	print('Chose SSH', ip_address)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# SFTP Actions
+@GeneticTree.declarePrimitive(ATTACKER, SFTP, ())
+def action0(self, input_nodes, context):
 	ip_address = context['ip address'] 	# we assume the provided context
 										# parameter is a Dict with 'ip address'
 	print('Chose SFTP', ip_address)
